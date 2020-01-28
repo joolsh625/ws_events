ac3d48f298d6b48fb326e0d577c376188344a8a1
Jools Hattey <jools.hg@gmail.com>
2020-01-28 15:21:53 +0000

    Started exercises

--- index.js
+++ index.js
@@ -2,3 +2,27 @@
 /**
  * Add your functions here...
  */
+
+function targetTextToConsole(event) {
+    console.log(event.target.textContent);
+}
+function tttcAttacher() {
+    window.button0.addEventListener("click", targetTextToConsole);
+}
+
+function lovelyParaAttacher() {
+    window.thisisalovelyparagraph.addEventListener("click", lovelyToggle);
+}
+
+function lovelyButtonAttacher() {
+    window.button1.addEventListener("click", lovelyToggle);
+}
+
+function concatAttacher() {
+    window.in1.addEventListener("change", concatAttacherEvent);
+    window.in2.addEventListener("change", concatAttacherEvent);
+}
+function concatAttacherEvent() {
+    window.out1.textContent += event.target.value;
+    event.target.removeEventListener("change", concatAttacherEvent);
+}
